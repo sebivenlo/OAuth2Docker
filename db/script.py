73b107c93a4bd49edb1dc9471056e1caab279638
Daljeet <daljeet.sandu@codecentric.de>
2018-11-29 20:15:36 +0100

    changed name of database in insert script

--- db/script.py
+++ db/script.py
@@ -10,6 +10,6 @@ with open('data.json') as json_data:
         print(str(i))
         print('\n')
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        response = requests.put('http://127.0.0.1:5984/database_name/' + str(i), data=json.dumps(user), headers=headers)
+        response = requests.put('http://127.0.0.1:5984/workshop_db/' + str(i), data=json.dumps(user), headers=headers)
         print(response.text)
         i = i + 1
